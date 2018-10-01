996f579539d25a5607938aeee8f93ab8cebac644
Victor Gerin de Lacerda <victor@victor-hd>
2018-10-01 01:50:13 -0300

    added the STUN proof of concept

--- main.cpp
+++ main.cpp
@@ -1,36 +1,13 @@
 #include <QCoreApplication>
 
-#include "test/testeclass.h"
-
-
-/*
- * This software é divided in 2 major parts
- * The StunClient and The Torrent Tracker
- *
- * Stun is a protocol used to let know the client he current
- * state is a way to discovery the NAT end orther thinks
- *
- * Go to stunclient.h to know more
- *
- *
- * The Torrent Tracker class with name "conn.h" is a simple implementation
- * of a udp client torrent tracker, the tracker here is used to dicovery
- * orther people and to annouce the current public endpoint (ip + udp port)
- *
- * Go to conn.h to know more
- *
- */
+#include "stunclient.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    new TesteClass();
-
-//    Conn c("udp://public.popcorn-tracker.org:6969");
-//    c.startConn();
+    StunClient s("udp://iphone-stun.strato-iphone.de:3478");
+//    StunClient s("udp://stun.12connect.com:3478");
 
     return a.exec();
 }
-
-
